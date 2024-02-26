584a2839cc77ae0d5943653a677e3e7bb75aeb07
UshanKaushalya <Ushanwk22@gmail.com>
2024-02-26 16:06:24 +0530

    start to work with toolkit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
 import './App.css';
-import {useDispatch, useSelector} from "react-redux";
-import {numberDecrement, numberIncrement} from "./store/reducers/number";
 
 function App() {
-
-    const number = useSelector(store => store.number);
-    const dispatch = useDispatch();
-
     return (
-        <div className="App">
-            <h1>{number}</h1>
-
-            <button onClick={() => {
-                dispatch(numberIncrement(1));
-            }}>Increment</button>
-
-            <button onClick={() => {
-                dispatch(numberDecrement(1));
-            }}>Decrement</button>
-
-        </div>
+        <>Hello</>
     );
 }
 
--- src/store/store.js
+++
@@ -1,7 +0,0 @@
-import {createStore} from "redux";
-import reducer from "./reducers/number";
-
-
-const store = createStore(reducer);
-
-export default store;
