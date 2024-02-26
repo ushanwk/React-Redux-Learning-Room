c255295e39ad489ab261941d5cd3723789c1f862
UshanKaushalya <Ushanwk22@gmail.com>
2024-02-26 13:57:57 +0530

    change valuew

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import './App.css';
 import UseRed from "./UseRed/UseRed";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import store from "./store/store";
 
 function App() {
 
     const number = useSelector(store => store.number);
+    const dispatch = useDispatch();
 
     return (
         <div className="App">
             <h1>{number}</h1>
+
+            <button onClick={() => {
+                dispatch({
+                    type: 'increment',
+                    payload: 1,
+                })
+            }}>Increment</button>
+
+            <button onClick={() => {
+                dispatch({
+                    type: 'decrement',
+                    payload: 1,
+                })
+            }}>Decrement</button>
+
         </div>
     );
 }
--- src/store/reducers/number.js
+++ src/store/reducers/number.js
@@ -3,6 +3,18 @@ const defaultState = {
 }
 
 const reducer = (state = defaultState, action) => {
+    switch (action.type) {
+        case 'increment':
+            return {
+                number: state.number + action.payload
+            }
+        case 'decrement':
+            return {
+                number: state.number - action.payload
+            }
+        default:
+            return state
+    }
     return state;
 }
 
